9c2d8f348f88ae18521d06b49ef272e4faa23b45
OK <38970774+okok147@users.noreply.github.com>
2026-09-24 16:47:01 +0800

    Restore original icon and bump Asympta Breathe to 1.2.0

--- asympta-breathe/build.sh
+++ asympta-breathe/build.sh
@@ -44,8 +44,8 @@ cat > "$CONTENTS/Info.plist" <<'PLIST'
   <key>CFBundleInfoDictionaryVersion</key><string>6.0</string>
   <key>CFBundleName</key><string>Asympta Breathe</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>1.1.0</string>
-  <key>CFBundleVersion</key><string>10</string>
+  <key>CFBundleShortVersionString</key><string>1.2.0</string>
+  <key>CFBundleVersion</key><string>20</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>LSUIElement</key><true/>
   <key>NSHighResolutionCapable</key><true/>
@@ -64,76 +64,96 @@ import UniformTypeIdentifiers
 let output = URL(fileURLWithPath: CommandLine.arguments[1])
 let w = 1024
 let h = 1024
-let colorSpace = CGColorSpaceCreateDeviceRGB()
-let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue
-let ctx = CGContext(data: nil, width: w, height: h, bitsPerComponent: 8, bytesPerRow: w * 4, space: colorSpace, bitmapInfo: bitmapInfo)!
+let cs = CGColorSpaceCreateDeviceRGB()
+let ctx = CGContext(
+    data: nil,
+    width: w,
+    height: h,
+    bitsPerComponent: 8,
+    bytesPerRow: w * 4,
+    space: cs,
+    bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+)!
 ctx.setAllowsAntialiasing(true)
 ctx.setShouldAntialias(true)
 
 let card = CGRect(x: 72, y: 72, width: 880, height: 880)
-let cardPath = CGPath(roundedRect: card, cornerWidth: 205, cornerHeight: 205, transform: nil)
+let cardPath = CGPath(
+    roundedRect: card,
+    cornerWidth: 205,
+    cornerHeight: 205,
+    transform: nil
+)
+
 ctx.saveGState()
 ctx.addPath(cardPath)
 ctx.clip()
 
-let paperColors = [
-    CGColor(red: 0.965, green: 0.945, blue: 0.900, alpha: 1),
-    CGColor(red: 0.915, green: 0.895, blue: 0.845, alpha: 1)
+let cream = [
+    CGColor(red: 0.985, green: 0.976, blue: 0.952, alpha: 1),
+    CGColor(red: 0.945, green: 0.932, blue: 0.900, alpha: 1)
 ] as CFArray
-let paperGradient = CGGradient(colorsSpace: colorSpace, colors: paperColors, locations: [0, 1])!
-ctx.drawLinearGradient(paperGradient, start: CGPoint(x: 0, y: 952), end: CGPoint(x: 1024, y: 72), options: [])
-
-var seed: UInt64 = 0xA59F_7D21_3C44_8B17
-func rnd() -> CGFloat {
-    seed = seed &* 6364136223846793005 &+ 1442695040888963407
-    return CGFloat((seed >> 33) & 0x7fffffff) / CGFloat(0x7fffffff)
-}
-for _ in 0..<11000 {
-    let x = card.minX + rnd() * card.width
-    let y = card.minY + rnd() * card.height
-    let len = 0.6 + rnd() * 3.0
-    let alpha = 0.018 + rnd() * 0.022
-    if rnd() > 0.5 {
-        ctx.setStrokeColor(CGColor(red: 0.42, green: 0.35, blue: 0.27, alpha: alpha))
-    } else {
-        ctx.setStrokeColor(CGColor(red: 1, green: 1, blue: 0.98, alpha: alpha * 1.7))
-    }
-    ctx.setLineWidth(0.55 + rnd() * 0.65)
-    ctx.move(to: CGPoint(x: x, y: y))
-    ctx.addLine(to: CGPoint(x: x + len, y: y + (rnd() - 0.5) * 1.4))
-    ctx.strokePath()
-}
-
-let center = CGPoint(x: 512, y: 540)
-let sphereColors = [
-    CGColor(red: 0.86, green: 0.91, blue: 0.92, alpha: 1),
-    CGColor(red: 0.46, green: 0.65, blue: 0.72, alpha: 1),
-    CGColor(red: 0.30, green: 0.50, blue: 0.60, alpha: 1)
-] as CFArray
-let sphereGradient = CGGradient(colorsSpace: colorSpace, colors: sphereColors, locations: [0, 0.55, 1])!
-ctx.drawRadialGradient(
-    sphereGradient,
-    startCenter: CGPoint(x: 440, y: 650), startRadius: 20,
-    endCenter: center, endRadius: 260,
-    options: [.drawsAfterEndLocation]
+let creamGradient = CGGradient(colorsSpace: cs, colors: cream, locations: [0, 1])!
+ctx.drawLinearGradient(
+    creamGradient,
+    start: CGPoint(x: 512, y: 952),
+    end: CGPoint(x: 512, y: 72),
+    options: []
 )
 
-ctx.setFillColor(CGColor(red: 0.965, green: 0.955, blue: 0.925, alpha: 1))
-ctx.fillEllipse(in: CGRect(x: 350, y: 235, width: 324, height: 324))
-ctx.setFillColor(CGColor(red: 0.42, green: 0.60, blue: 0.67, alpha: 1))
-ctx.fillEllipse(in: CGRect(x: 325, y: 338, width: 374, height: 345))
+let orbRect = CGRect(x: 220, y: 230, width: 584, height: 584)
+let blues = [
+    CGColor(red: 0.83, green: 0.91, blue: 0.95, alpha: 1),
+    CGColor(red: 0.45, green: 0.68, blue: 0.80, alpha: 1),
+    CGColor(red: 0.30, green: 0.55, blue: 0.70, alpha: 1)
+] as CFArray
+let orbGradient = CGGradient(colorsSpace: cs, colors: blues, locations: [0, 0.58, 1])!
+ctx.saveGState()
+ctx.addEllipse(in: orbRect)
+ctx.clip()
+ctx.drawRadialGradient(
+    orbGradient,
+    startCenter: CGPoint(x: 405, y: 690),
+    startRadius: 24,
+    endCenter: CGPoint(x: 525, y: 525),
+    endRadius: 330,
+    options: [.drawsAfterEndLocation]
+)
+ctx.restoreGState()
 
-ctx.setFillColor(CGColor(red: 1, green: 0.995, blue: 0.97, alpha: 1))
-ctx.fillEllipse(in: CGRect(x: 705, y: 710, width: 86, height: 86))
+let smallRect = CGRect(x: 401, y: 257, width: 222, height: 222)
+let small = [
+    CGColor(red: 0.985, green: 0.99, blue: 0.985, alpha: 1),
+    CGColor(red: 0.88, green: 0.92, blue: 0.94, alpha: 1)
+] as CFArray
+let smallGradient = CGGradient(colorsSpace: cs, colors: small, locations: [0, 1])!
+ctx.saveGState()
+ctx.addEllipse(in: smallRect)
+ctx.clip()
+ctx.drawRadialGradient(
+    smallGradient,
+    startCenter: CGPoint(x: 475, y: 405),
+    startRadius: 10,
+    endCenter: CGPoint(x: 520, y: 360),
+    endRadius: 145,
+    options: [.drawsAfterEndLocation]
+)
+ctx.restoreGState()
 
 ctx.restoreGState()
+
 ctx.addPath(cardPath)
-ctx.setStrokeColor(CGColor(red: 0.62, green: 0.58, blue: 0.50, alpha: 0.12))
+ctx.setStrokeColor(CGColor(red: 0.58, green: 0.55, blue: 0.50, alpha: 0.10))
 ctx.setLineWidth(2)
 ctx.strokePath()
 
 let image = ctx.makeImage()!
-let dest = CGImageDestinationCreateWithURL(output as CFURL, UTType.png.identifier as CFString, 1, nil)!
+let dest = CGImageDestinationCreateWithURL(
+    output as CFURL,
+    UTType.png.identifier as CFString,
+    1,
+    nil
+)!
 CGImageDestinationAddImage(dest, image, nil)
 precondition(CGImageDestinationFinalize(dest))
 SWIFT
@@ -157,6 +177,6 @@ codesign --verify --deep --strict --verbose=2 "$APP"
 plutil -lint "$CONTENTS/Info.plist"
 
 mkdir -p "$OUT/package"
-ditto -c -k --sequesterRsrc --keepParent "$APP" "$OUT/package/Asympta-Breathe-1.1.0.zip"
+ditto -c -k --sequesterRsrc --keepParent "$APP" "$OUT/package/Asympta-Breathe-1.2.0.zip"
 
-echo "Built $OUT/package/Asympta-Breathe-1.1.0.zip"
+echo "Built $OUT/package/Asympta-Breathe-1.2.0.zip"
