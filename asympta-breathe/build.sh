006b44a90adb7d2dd849ea0b15f7488b0be2183b
OK <38970774+okok147@users.noreply.github.com>
2026-09-24 17:27:46 +0800

    Build Asympta Breathe 1.5.0

--- asympta-breathe/build.sh
+++ asympta-breathe/build.sh
@@ -45,8 +45,8 @@ cat > "$CONTENTS/Info.plist" <<'PLIST'
   <key>CFBundleInfoDictionaryVersion</key><string>6.0</string>
   <key>CFBundleName</key><string>Asympta Breathe</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>1.4.1</string>
-  <key>CFBundleVersion</key><string>41</string>
+  <key>CFBundleShortVersionString</key><string>1.5.0</string>
+  <key>CFBundleVersion</key><string>50</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>LSUIElement</key><true/>
   <key>NSHighResolutionCapable</key><true/>
@@ -178,6 +178,6 @@ codesign --verify --deep --strict --verbose=2 "$APP"
 plutil -lint "$CONTENTS/Info.plist"
 
 mkdir -p "$OUT/package"
-ditto -c -k --sequesterRsrc --keepParent "$APP" "$OUT/package/Asympta-Breathe-1.4.1.zip"
+ditto -c -k --sequesterRsrc --keepParent "$APP" "$OUT/package/Asympta-Breathe-1.5.0.zip"
 
-echo "Built $OUT/package/Asympta-Breathe-1.4.1.zip"
+echo "Built $OUT/package/Asympta-Breathe-1.5.0.zip"
