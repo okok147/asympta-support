2093851d01100e5f063f83e4cac5f0db6b277c2f
OK <38970774+okok147@users.noreply.github.com>
2026-09-24 23:00:21 +0800

    Build Asympta Breathe 1.9.0

--- asympta-breathe/build.sh
+++ asympta-breathe/build.sh
@@ -46,8 +46,8 @@ cat > "$CONTENTS/Info.plist" <<'PLIST'
   <key>CFBundleInfoDictionaryVersion</key><string>6.0</string>
   <key>CFBundleName</key><string>Asympta Breathe</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>1.8.0</string>
-  <key>CFBundleVersion</key><string>80</string>
+  <key>CFBundleShortVersionString</key><string>1.9.0</string>
+  <key>CFBundleVersion</key><string>90</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>LSUIElement</key><true/>
   <key>NSHighResolutionCapable</key><true/>
@@ -179,6 +179,6 @@ codesign --verify --deep --strict --verbose=2 "$APP"
 plutil -lint "$CONTENTS/Info.plist"
 
 mkdir -p "$OUT/package"
-ditto -c -k --sequesterRsrc --keepParent "$APP" "$OUT/package/Asympta-Breathe-1.8.0.zip"
+ditto -c -k --sequesterRsrc --keepParent "$APP" "$OUT/package/Asympta-Breathe-1.9.0.zip"
 
-echo "Built $OUT/package/Asympta-Breathe-1.8.0.zip"
+echo "Built $OUT/package/Asympta-Breathe-1.9.0.zip"
